1f8f7aa7b1d893216c675288adcd5ab30d59d80e
Presa <ivopresa@gmail.com>
2023-10-03 02:48:25 +0200

    Compression options, better compression logic to ensure high quality images, dialog on completion,  file names according to compression options, better logs,  better user info,  better user experience

--- compress_images.py
+++ compress_images.py
@@ -3,42 +3,78 @@ import shutil
 from PIL import Image
 import wx
 import sys
+import subprocess
 
 Image.MAX_IMAGE_PIXELS = None
+
+COMPRESSION_OPTIONS = [
+    'Compress with No Data Loss',
+    'Compress Size x2',
+    'Compress Size x4',
+    'Compress Size x8',
+    'Compress Size x16',
+]
 
 
 def bytes_to_mb(size_in_bytes):
     return size_in_bytes / (1024 * 1024)
 
 
-def compress_image(img_path):
+def compress_image(img_path, compression_option):
     initial_size = os.path.getsize(img_path)
-    with Image.open(img_path) as img:
-        width, height = img.size
+    try:
+        with Image.open(img_path) as img:
+            width, height = img.size
 
-        # Resize only if width exceeds 1080
-        if width > 1080:
-            aspect_ratio = height / width
-            new_width = 1080
-            new_height = int(aspect_ratio * new_width)
+            # If compression option involves resizing, calculate new dimensions
+            if 'Compress Size' in compression_option:
+                factor = int(compression_option.split(' ')[-1][1:])
+                new_width = int(width / factor)
+                aspect_ratio = height / width
+                new_height = int(aspect_ratio * new_width)
+                # Check the image mode and decide the resampling method
+                if img.mode in ["L", "RGB", "RGBA"]:
+                    resampling_method = Image.Resampling.LANCZOS
+                else:
+                    resampling_method = Image.NEAREST
 
-            # If the image is a .tif file, handle it differently
-            if img_path.lower().endswith('.tif'):
-                try:
-                    img = img.resize((new_width, new_height))
-                    img.save(img_path)
-                except Exception as e:
-                    print(f"Error processing {img_path}: {e}")
-                    return initial_size, initial_size
+                img = img.resize((new_width, new_height), resampling_method)
+
+            # Apply appropriate compression
+            if img_path.lower().endswith('.png'):
+                img.save(img_path, optimize=True)
+            elif img_path.lower().endswith(('.jpg', '.jpeg')):
+                img.save(img_path, quality=95)
+            elif img_path.lower().endswith(('.tif', '.tiff')):
+                img.save(img_path, compression='tiff_lzw')
             else:
-                img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
-                img.save(img_path, quality=100)
+                print("")
+                print(f"{img_path} has an unsupported file extension. Skipping...")
+                print("")
+                return initial_size, initial_size
+    except Exception as e:
+        print("")
+        print(f"Error processing {img_path}: {e}")
+        return initial_size, initial_size
 
     final_size = os.path.getsize(img_path)
     return initial_size, final_size
 
 
-def process_directory(src_dir, dest_dir):
+def get_new_filename(img_path, compression_option):
+    base, ext = os.path.splitext(img_path)
+    if compression_option == 'Compress with No Data Loss':
+        new_name = base + '_compressed' + ext
+    elif 'Compress Size' in compression_option:
+        factor = compression_option.split(' ')[-1]
+        new_name = base + '_compressed_' + factor + ext
+    # In case any problems with compression_option
+    else:
+        new_name = base + '_compressed' + ext
+    return new_name
+
+
+def process_directory(src_dir, dest_dir, compression_option):
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
     shutil.copytree(src_dir, dest_dir)
@@ -52,7 +88,7 @@ def process_directory(src_dir, dest_dir):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.tif', '.tiff')):
                 img_path = os.path.join(root, file)
-                initial_size, final_size = compress_image(img_path)
+                initial_size, final_size = compress_image(img_path, compression_option)
 
                 if initial_size > final_size:
                     num_files_compressed += 1
@@ -61,8 +97,7 @@ def process_directory(src_dir, dest_dir):
                 total_saved_size += saved_size
                 num_files_processed += 1
 
-                base, ext = os.path.splitext(img_path)
-                new_name = base + '_compressed' + ext
+                new_name = get_new_filename(img_path, compression_option)
                 os.rename(img_path, new_name)
 
                 # Logging info for each file
@@ -71,22 +106,122 @@ def process_directory(src_dir, dest_dir):
                 print(log_entry)
                 wx.Yield()  # Process any pending events, allowing the GUI to update
 
-    with open("log.txt", "w") as log_file:
+    # Create log file
+    log_file_path = os.path.join(dest_dir, "log.txt")
+    with open(log_file_path, "w") as log_file:
         log_file.write(f"Processed {num_files_processed} files:\n")
         log_file.write('\n'.join(log_entries))
+        log_file.write("\n")
         log_file.write(f"\nSuccessfully compressed {num_files_compressed} images.")
         log_file.write(f"\n{num_files_processed - num_files_compressed} images were not compressed.")
         log_file.write(f"\nIn total, we saved {bytes_to_mb(total_saved_size):.2f} MB")
 
+    return log_file_path
+
+
+def count_files_in_destination(directory):
+    total_files = 0
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            total_files += 1
+
+    if total_files == 0:
+        print("Destination Directory is empty, that is perfect!")
+        print("")
+    else:
+        print(f"Total Files in Destination Directory: {total_files}")
+        print("Warning: It is recommended that the destination directory should be empty.")
+        print("Warning: Existing files in the destination will be lost.")
+        print("")
+
+
+def count_files_in_source(directory):
+    total_files = 0
+    unsupported_files_count = 0
+    supported_extensions = ['.png', '.jpg', '.jpeg', '.tif', '.tiff']
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            total_files += 1
+            print(f"File {total_files}: {file}")
+            if not any(file.lower().endswith(ext) for ext in supported_extensions):
+                unsupported_files_count += 1
+
+    print(f"Total Files in Source Directory: {total_files}")
+    print("")
+    if unsupported_files_count > 0:
+        print(f"Unsupported Files in Source Directory: {unsupported_files_count}")
+        print(f"Note: Only files with the following extensions will be compressed: {', '.join(supported_extensions)}")
+        print("")
+
+
+# Dialog with option to open log.txt
+class CustomDialog(wx.Dialog):
+    def __init__(self, *args, log_file_path, source_directory, destination_directory, **kwargs):
+        super(CustomDialog, self).__init__(*args, **kwargs)
+        self.log_file_path = log_file_path
+        self.source_directory = source_directory
+        self.destination_directory = destination_directory
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        message = wx.StaticText(self, label="Compression of images has completed!")
+        sizer.Add(message, wx.SizerFlags().Border(wx.TOP | wx.LEFT, 10))
+
+        # Button to open the log file
+        open_log_btn = wx.Button(self, label="Open Log File")
+        open_log_btn.Bind(wx.EVT_BUTTON, self.open_log_file)
+        sizer.Add(open_log_btn, wx.SizerFlags().Border(wx.ALL, 10))
+
+        # Button to open Source Directory
+        open_src_btn = wx.Button(self, label="Open Source Directory")
+        open_src_btn.Bind(wx.EVT_BUTTON, self.open_source_directory)
+
+        # Button to open Destination Directory
+        open_dest_btn = wx.Button(self, label="Open Destination Directory")
+        open_dest_btn.Bind(wx.EVT_BUTTON, self.open_destination_directory)
+
+        # Buttons Source and Destination together
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        button_sizer.Add(open_src_btn, 0, wx.RIGHT, 10)
+        button_sizer.Add(open_dest_btn, 0, wx.LEFT, 10)
+        sizer.Add(button_sizer, 0, wx.CENTER | wx.ALL, 10)
+
+        ok_button = wx.Button(self, wx.ID_OK, "OK")
+        sizer.Add(ok_button, wx.SizerFlags().Border(wx.ALL, 10).Center())
+
+        self.SetSizer(sizer)
+        self.Fit()
+
+    def open_log_file(self, event):
+        if sys.platform == 'win32':
+            os.startfile(self.log_file_path)
+        elif sys.platform == 'darwin':
+            subprocess.call(['open', self.log_file_path])
+        else:
+            subprocess.call(['xdg-open', self.log_file_path])
+
+    def open_source_directory(self, event):
+        self.open_directory(self.source_directory)
+
+    def open_destination_directory(self, event):
+        self.open_directory(self.destination_directory)
+
+    def open_directory(self, directory_path):
+        if sys.platform == 'win32':
+            os.startfile(directory_path)
+        elif sys.platform == 'darwin':
+            subprocess.call(['open', directory_path])
+        else:
+            subprocess.call(['xdg-open', directory_path])
+
 
 class CompressorApp(wx.Frame):
     def __init__(self, parent, title):
-        super(CompressorApp, self).__init__(parent, title=title, size=(1000, 500))
+        super(CompressorApp, self).__init__(parent, title=title, size=(1300, 800))
 
         self.source_directory = None
         self.destination_directory = None
-
-        # Create a panel in the frame
         self.panel = wx.Panel(self)
 
         # Determine if we're running as a bundled executable
@@ -105,11 +240,18 @@ class CompressorApp(wx.Frame):
         self.panel.Bind(wx.EVT_PAINT, self.on_paint)
 
         # Add an explanation label at the top
-        explanation = ("This script compresses all images in the selected source directory and saves them \n"
-                       "in the selected destination directory. To use:\n"
-                       "1. Select the source directory.\n"
-                       "2. Select the destination directory.\n"
-                       "3. Click 'Start Compression'.")
+        explanation = (
+            "This script compresses all images in the selected source directory and saves them "
+            "in the selected destination directory. To use:\n"
+            "1. Select the source directory.\n"
+            "2. Select the destination directory.\n"
+            "3. Select a compression option. (Default is 'Compress with No Data Loss').\n"
+            "4. Click 'Start Compression'.\n\n"
+            "Note on compression options:\n"
+            "- 'Compress with No Data Loss' retains the original image size but tries to reduce the file size without quality loss.\n"
+            "- 'Compress Size x2' resizes the image to half its original dimensions while maintaining the aspect ratio. This usually results in a significant file size reduction with little to no perceptible quality loss.\n"
+            "- As you increase the compression size (x4, x8, x16), the image size decreases proportionally. Quality loss becomes more noticeable, especially with 'Compress Size x16'."
+        )
         self.explanation_label = wx.StaticText(self.panel, label=explanation)
 
         # Add buttons to the panel
@@ -154,6 +296,10 @@ class CompressorApp(wx.Frame):
         # Start button
         main_sizer.Add(self.btn_start, 0, wx.CENTER | wx.BOTTOM, 10)
 
+        # Add a Choice widget for compression options
+        self.compression_choice = wx.Choice(self.panel, choices=COMPRESSION_OPTIONS, pos=(500, 50))
+        self.compression_choice.SetSelection(0)  # Set default selection to the first option
+
         # Set the main sizer for the panel
         self.panel.SetSizer(main_sizer)
         self.Centre()
@@ -171,6 +317,7 @@ class CompressorApp(wx.Frame):
 
         # Draw the bitmap onto the panel
         dc.DrawBitmap(bmp, 0, 0, True)
+
     class TextRedirector:
         """A helper class to redirect the stdout/stderr to the TextCtrl widget."""
 
@@ -200,6 +347,7 @@ class CompressorApp(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             self.source_directory = dlg.GetPath()
             print(f"Selected Source Directory: {self.source_directory}")
+            count_files_in_source(self.source_directory)
         dlg.Destroy()
 
     def on_select_destination(self, event):
@@ -207,13 +355,28 @@ class CompressorApp(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             self.destination_directory = dlg.GetPath()
             print(f"Selected Destination Directory: {self.destination_directory}")
+            count_files_in_destination(self.destination_directory)
         dlg.Destroy()
 
     def on_start_compression(self, event):
         if self.source_directory and self.destination_directory:
-            print("Starting the compression!")
-            wx.Yield()
-            process_directory(self.source_directory, self.destination_directory)
+            if self.source_directory == self.destination_directory:
+                wx.MessageBox('Source and Destination directories cannot be the same. Please select a different '
+                              'destination directory.', 'Info', wx.OK | wx.ICON_INFORMATION)
+            # starting compression
+            else:
+                # Get selected compression option
+                compression_option = self.compression_choice.GetString(self.compression_choice.GetSelection())
+                print(f"Starting the compression with option: {compression_option}!")
+                wx.Yield()
+                log_file_path = process_directory(self.source_directory, self.destination_directory, compression_option)
+
+                # Custom Message Box on completion
+                dlg = CustomDialog(self, title="Compression Completed", log_file_path=log_file_path,
+                                   source_directory=self.source_directory,
+                                   destination_directory=self.destination_directory)
+                dlg.ShowModal()
+                dlg.Destroy()
         else:
             wx.MessageBox('Please select both source and destination directories first.', 'Info',
                           wx.OK | wx.ICON_INFORMATION)
