01a3313bef3436d25d7d75f3e63d984159d78b49
Presa <ivopresa@gmail.com>
2023-12-02 15:11:27 +0100

    setup.py&icon

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine-tuning.
 build_exe_options = {
-    "packages": ["os", "shutil", "sys", "webbrowser", "wx", "wx.adv", "PIL"],
+    "packages": ["os", "shutil", "sys", "webbrowser", "subprocess", "platform", "wx", "wx.adv", "PIL", "wx.lib.delayedresult", "wx.lib.buttons"],
     "include_files": [
-        ("./img/background.jpg", "img/background.jpg"),
+        ("./img/background2.png", "img/background2.png"),
         ("./img/github_icon.gif", "img/github_icon.gif"),
         ("./img/busy_loading.gif", "img/busy_loading.gif")
     ],
@@ -17,9 +17,9 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name="YourAppName",
-    version="0.1",
-    description="Your application description",
+    name="zenWai_Compress_images",
+    version="0.3",
+    description="Compress images from a source to a destination directory",
     options={"build_exe": build_exe_options},
     executables=[Executable("compress_images.py", base=base, icon="./img/icon.ico")]
 )
